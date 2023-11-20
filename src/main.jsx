67377d2eeaa7d80dda3688d1502d0b44e94d13dc
rootnure <nur.diu.2791@gmail.com>
2023-11-20 20:04:14 +0600

    success msg added for successful add to cart using toast

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import { router } from './Routes/Routes';
-import { HelmetProvider } from 'react-helmet-async';
-import AuthProvider from './providers/AuthProvider';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { router } from "./Routes/Routes";
+import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./providers/AuthProvider";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <HelmetProvider>
-        <div className='max-w-[1920px] mx-auto'>
-          <ToastContainer />
+        <div className="max-w-[1920px] mx-auto">
+          <ToastContainer
+            position="top-center"
+            autoClose={3500}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss={false}
+            draggable={false}
+            pauseOnHover
+            theme="light"
+          />
           <RouterProvider router={router} />
         </div>
       </HelmetProvider>
     </AuthProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/pages/Shared/FoodCard.jsx
+++ src/pages/Shared/FoodCard.jsx
@@ -4,68 +4,77 @@ import useAuthHook from "../../hooks/useAuthHook";
 import Swal from "sweetalert2";
 import { useLocation, useNavigate } from "react-router-dom";
 import { axiosSecure } from "../../hooks/useAxiosSecure";
+import { toast } from "react-toastify";
 
 const FoodCard = ({ item }) => {
-    const {user} = useAuthHook();
-    const navigate = useNavigate();
-    const location = useLocation();
-    const { _id, name, recipe, image, price } = item;
+  const { user } = useAuthHook();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { _id, name, recipe, image, price } = item;
 
-    const handleAddToCart = () => {
-        if(user && user?.email) {
-            const cartItem = {
-                menuId: _id,
-                email: user.email,
-                name,
-                recipe,
-                image,
-                price
-            }
-            axiosSecure.post("/cart", cartItem)
-            .then(res => {
-                console.log(res.data);
-            })
-            .catch(err => console.error(err))
-        } else {
-            Swal.fire({
-                title: "You are not logged in",
-                text: "Do you want to login now?",
-                icon: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#3085d6",
-                cancelButtonColor: "#d33",
-                confirmButtonText: "Yes, Go To Login"
-              }).then((result) => {
-                if (result.isConfirmed) {
-                  navigate("/login", {state: {from: location}, replace: true})
-                }
-              });
+  const handleAddToCart = () => {
+    if (user && user?.email) {
+      const cartItem = {
+        menuId: _id,
+        email: user.email,
+        name,
+        recipe,
+        image,
+        price,
+      };
+      axiosSecure
+        .post("/cart", cartItem)
+        .then((res) => {
+          if (res.data.insertedId) {
+            toast.success(`${name} successfully added to cart`);
+          }
+        })
+        .catch((err) => console.error(err));
+    } else {
+      Swal.fire({
+        title: "You are not logged in",
+        text: "Do you want to login now?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, Go To Login",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login", { state: { from: location }, replace: true });
         }
+      });
     }
-    return (
-        <>
-            <div className="card bg-base-100 rounded-xl relative">
-                {price ? <p className="absolute right-2 top-2 px-1 py-0.5 rounded text-white bg-blue-600 font-medium">${price.toFixed(2)}</p> : ""}
-                <figure className="h-60 rounded-t-xl overflow-hidden">
-                    <img src={image} alt={name} className="min-w-full min-h-full" />
-                </figure>
-                <div className="card-body bg-gray-100 items-center text-center rounded-b-xl">
-                    <h2 className="card-title text-2xl font-bold">{name}</h2>
-                    <p>{recipe}</p>
-                    <div className="card-actions">
-                        <MainBtn 
-                        handleAddToCart={handleAddToCart}
-                        isBgWhite>Add To Cart</MainBtn>
-                    </div>
-                </div>
-            </div>
-
-        </>
-    );
+  };
+  return (
+    <>
+      <div className="card bg-base-100 rounded-xl relative">
+        {price ? (
+          <p className="absolute right-2 top-2 px-1 py-0.5 rounded text-white bg-blue-600 font-medium">
+            ${price.toFixed(2)}
+          </p>
+        ) : (
+          ""
+        )}
+        <figure className="h-60 rounded-t-xl overflow-hidden">
+          <img src={image} alt={name} className="min-w-full min-h-full" />
+        </figure>
+        <div className="card-body bg-gray-100 items-center text-center rounded-b-xl">
+          <h2 className="card-title text-2xl font-bold">{name}</h2>
+          <p>{recipe}</p>
+          <div className="card-actions">
+            <MainBtn handleAddToCart={handleAddToCart} isBgWhite>
+              Add To Cart
+            </MainBtn>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 FoodCard.propTypes = {
-    item: PropTypes.object.isRequired
-}
+  item: PropTypes.object.isRequired,
+};
 
 export default FoodCard;
